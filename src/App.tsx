2305ed4b80d3d1bae731e07fb4052c36d49def78
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-17 20:32:41 -0800

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { useToggle } from './hooks/useToggle';
+import Board from './components/Board';
+import { SquareType } from './types';
+import { generateSquares } from './utils/generateSquares';
 
 const App: React.FC = () => {
+  const [userName, setUserName] = useLocalStorage<string>('username', '');
+  const [currentPlayer, toggleCurrentPlayer] = useToggle<'X' | 'O'>('X', 'O');
+
+  const [squares, setSquares] = useState<SquareType[]>(generateSquares());
+
+  const squareSelected = (id: number) => {
+    console.log(`Square ${id} clicked!`);
+
+    const newSquares = squares.map((square) => {
+      if (square.id === id && !square.value) {
+        const newValue = currentPlayer;
+        toggleCurrentPlayer();
+        return { ...square, value: newValue };
+      }
+      return square;
+    });
+    setSquares(newSquares);
+  };
+
   return (
     <div className="App">
       <header>
-        <nav></nav>
+        <nav className="tic-tac-toe__nav">
+          <ul className="tic-tac-toe__nav__menu">
+            <li className="tic-tac-toe__nav__menu__item">
+              <button>Home</button>
+            </li>
+            <li className="tic-tac-toe__nav__menu__item">
+              <button>Log in</button>
+            </li>
+            <li className="tic-tac-toe__nav__menu__item">
+              <button>Find a game</button>
+            </li>
+          </ul>
+        </nav>
+        <h1>Multi-player Tic Tac Toe</h1>
       </header>
-      <main></main>
+      <main>
+        <Board squares={squares} onClickCallback={squareSelected} />
+      </main>
       <footer></footer>
     </div>
   );
