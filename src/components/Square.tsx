5cebca3fb0e19e02a53ddce332ee40fc22e18a69
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-17 20:26:55 -0800

    Square component (ugly color)

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,18 @@
-const Square = () => {
-  return <div />;
+import React from 'react';
+import './Square.css';
+
+interface SquareProps {
+  id: number;
+  value: string | null;
+  onClickCallback: (id: number) => void;
+}
+
+const Square = ({ id, value, onClickCallback }: SquareProps) => {
+  return (
+    <button data-testid={`square-${id}`} className="square" onClick={() => onClickCallback(id)}>
+      {value ? <div className="square__value">{value}</div> : ''}
+    </button>
+  );
 };
 
 export default Square;
