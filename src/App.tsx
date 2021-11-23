c2618159ec551b95770bf45914d59f7d9797a218
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-22 18:35:12 -0800

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -12,6 +12,7 @@ import { generateSquares } from './utils/generateSquares';
 import Login from './components/Login';
 
 const App: React.FC = () => {
+  const [gameId, setGameId] = useState<string | null>(null);
   const [userName, setUserName] = useLocalStorage<string>('username', '');
   const [currentPlayer, toggleCurrentPlayer] = useToggle<'X' | 'O'>('X', 'O');
 
@@ -31,9 +32,12 @@ const App: React.FC = () => {
     setSquares(newSquares);
   };
 
-  const notifyDeparture = (oldGameId: string, message: string) => {};
+  const notifyDeparture = (oldGameId: string, message: string) => {
+    console.log(oldGameId);
+    console.log(message);
+  };
 
-  const updateGameId = (id: string) => {
+  const updateGameId = (id: string | null) => {
     if (gameId) {
       notifyDeparture(gameId, 'Leaving the Game for another');
     }
@@ -60,6 +64,13 @@ const App: React.FC = () => {
             {userName !== '' ? (
               <li className="tic-tac-toe__nav__menu__item">
                 <Button href="/find-game">Find a game</Button>
+              </li>
+            ) : (
+              ''
+            )}
+            {gameId ? (
+              <li className="tic-tac-toe__nav__menu__item">
+                <Button onClick={() => updateGameId(null)}>Leave Game</Button>
               </li>
             ) : (
               ''
