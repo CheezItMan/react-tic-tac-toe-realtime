a567ed3ed4c462ca89c2fcbd09dbf64be2b87ce3
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-20 18:11:19 -0800

    useLogin Hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { API_URL } from '../utils/constants';
 
-export function useLogin(): [string, (username: string) => void] {
+export default function useLogin(): [
+  string,
+  (username: string) => void,
+  () => void,
+] {
   const [username, setUsername] = useState<string>('');
 
   const performLogin = async (newUsername: string): Promise<void> => {
     try {
-      const response = await axios.post('URL', {
-        username: newUsername,
+      console.log(API_URL);
+      const response = await axios.post(`${API_URL}/player/v1`, {
+        name: newUsername,
       });
-      if (response.status === 200) {
+      if (response.status === 200 || response.status === 201) {
         // on success set state
         setUsername(newUsername);
       }
@@ -18,5 +24,9 @@ export function useLogin(): [string, (username: string) => void] {
     }
   };
 
-  return [username, performLogin];
+  const performLogout = () => {
+    setUsername('');
+  };
+
+  return [username, performLogin, performLogout];
 }
