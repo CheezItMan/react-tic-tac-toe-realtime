b703b7a272292d214f65e34ff1303be0217febdc
Chris McAnally <chris@adadevelopersacademy.org>
2022-02-13 19:28:25 -0800

    createGame view starter

--- src/components/CreateGame.tsx
+++ src/components/CreateGame.tsx
@@ -1,63 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { TicTackToeSocketType } from '../utils/SocketService';
 
 interface CreateGameProps {
   userName: string;
   onCreateGameCallback: (gameId: string) => void;
+  socket: TicTackToeSocketType;
 }
 
 enum GameStatus {
-  LOADING,
-  STARTED,
-  WAITING_FOR_PLAYERS,
-  CANCELLED,
+  LOADING = 'Loading Game',
+  STARTED = 'Started',
+  WAITING_FOR_PLAYERS = 'Waiting for players',
+  CANCELLED = 'Cancelling',
 }
 
-const CreateGame: React.FC<CreateGameProps> = ({ userName, onCreateGameCallback }) => {
+const CreateGame: React.FC<CreateGameProps> = ({ userName, onCreateGameCallback, socket }) => {
   const [gameState, setGameState] = useState<GameStatus>(GameStatus.LOADING);
-
   const navigate = useNavigate();
-
-  const onCancel = () => {
-    setGameState(GameStatus.CANCELLED);
-  };
-
   useEffect(() => {
+    const onGameCreated = (gameId: string) => {
+      onCreateGameCallback(gameId);
+      setGameState(GameStatus.STARTED);
+    };
+
     // place to put socketio code
     if (gameState === GameStatus.LOADING) {
-      setGameState(GameStatus.WAITING_FOR_PLAYERS);
+      socket.emit('createGame', userName);
+      // just don't listen for GameCreated event twice
+      socket.off('gameCreated');
+      socket.on('gameCreated', onGameCreated);
     } else if (gameState === GameStatus.CANCELLED) {
       navigate('/');
     }
-
     return () => {
-      // Place to put closing socketio code
+      socket.off('gameCreated');
     };
-  }, [gameState, navigate]);
+  }, [gameState, navigate, socket, userName, onCreateGameCallback]);
 
-  if (gameState === GameStatus.LOADING) {
-    return (
-      <div>
-        <h3>Loading...</h3>
-        <Button onClick={onCancel}>Cancel</Button>
-      </div>
-    );
-  } else if (gameState === GameStatus.WAITING_FOR_PLAYERS) {
-    return (
-      <div>
-        <h3>Waiting for players...</h3>
-        <Button onClick={onCancel}>Cancel</Button>
-      </div>
-    );
-  } else {
-    navigate('/game');
-    return (
-      <div>
-        <h1>Starting!</h1>
-      </div>
-    );
+  if (!userName) {
+    navigate('/');
+    return <div />;
   }
+
+  const onCancel = () => {
+    setGameState(GameStatus.CANCELLED);
+    console.log('Cancelling');
+  };
+
+  return (
+    <div>
+      <h3>Waiting for players...</h3>
+      <p>Status {gameState}</p>
+      <Button onClick={onCancel}>Cancel</Button>
+    </div>
+  );
 };
 
 export default CreateGame;
