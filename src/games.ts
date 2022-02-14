d267e68fc23d792018dbd68453dcb1288fbc0c91
Chris McAnally <chris@adadevelopersacademy.org>
2022-02-13 19:27:00 -0800

    dummy list of games

---
+++ src/games.ts
@@ -0,0 +1,26 @@
+import { GameInfo } from './types';
+
+const games: GameInfo[] = [
+  {
+    id: '1',
+    host: 'Bozo the Clown',
+    status: 'waiting',
+  },
+  {
+    id: '2',
+    host: 'Elon Musk',
+    status: 'finished',
+  },
+  {
+    id: '3',
+    host: 'Roshni',
+    status: 'waiting',
+  },
+  {
+    id: '4',
+    host: 'Chris',
+    status: 'started',
+  },
+];
+
+export default games;
